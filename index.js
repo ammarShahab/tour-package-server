88de3e743e42969f9248c53c4bd1db86b21e170e
Ashahab007 <ashahab007@gmail.com>
2025-06-17 04:17:44 +0600

    hide environment variables and deploy in vercel successfully

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; */
 
-const uri =
-  "mongodb+srv://tripNestdb:RNHIaMGFYGlF7DXG@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+/* DB_USER=tripNestdb
+DB_PASS=RNHIaMGFYGlF7DXG */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const tourPackagesCollections = client
       .db("tripNestdb")
@@ -88,7 +89,7 @@ async function run() {
         package_details,
         guide_contact_no,
       } = req.body;
-      console.log(req.body);
+      // console.log(req.body);
       const updatedPackage = {
         $set: {
           tour_name: tour_name,
@@ -163,11 +164,6 @@ async function run() {
     app.patch("/bookings/:id", async (req, res) => {
       const { id } = req.params;
       const { status } = req.body;
-      /* const updatedDoc = {
-        $set: {
-          status: "completed",
-        },
-      }; */
 
       const result = await myBookingsCollections.updateOne(
         {
@@ -183,9 +179,9 @@ async function run() {
     });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log(
+    /* console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    ); */
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
